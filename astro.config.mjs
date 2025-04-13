0f642ebb965455586295dcab9b7d5b64f1575558
laisky0321 <laiweimin0321@gmail.com>
2025-04-13 16:32:40 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
   output: 'server', // 确保使用服务端渲染
+  adapter: cloudflare(),
   devToolbar: { enabled: false }, // 关闭开发工具栏
   server: {
     headers: {
